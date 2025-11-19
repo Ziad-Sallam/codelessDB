6d026400c6a825a7156de1b616a86d0140e1b8bd
Ahmed-Ragy-3 <ahmedragy2005@gmail.com>
2025-11-19 15:45:24 +0200

    fix extra Backend directory

---
+++ backend/src/main/java/backend/security/JwtAuthenticationFilter.java
@@ -0,0 +1,56 @@
+package backend.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.substring(7);
+
+        int userId = jwtUtil.extractSubject(token);
+        String username = jwtUtil.extractUsername(token);
+
+        AuthUser authUser = new AuthUser(userId, username);
+
+        if (SecurityContextHolder.getContext().getAuthentication() == null) {
+            if (jwtUtil.isTokenValid(token, userId)) {
+                UsernamePasswordAuthenticationToken auth =
+                        new UsernamePasswordAuthenticationToken(
+                                authUser,
+                                null,
+                                null // empty authorities (or load from DB)
+                        );
+
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
