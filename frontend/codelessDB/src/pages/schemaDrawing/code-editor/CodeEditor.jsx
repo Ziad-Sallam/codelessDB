99806878efb45c9bf36834abb79347451921a291
noureldien20 <143279186+noureldien20@users.noreply.github.com>
2025-11-26 01:09:29 +0200

    updated code-editor to work with schema.jsx

---
+++ frontend/codelessDB/src/pages/schemaDrawing/code-editor/CodeEditor.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import { sql } from '@codemirror/lang-sql';
+import './CodeEditor.css';
+
+function CodeEditor({ initialCode, onClose }) {
+
+  const [code, setCode] = useState(initialCode || '');
+
+  return (
+    <div className='code-container'>
+
+      <div className='code-editor-wrapper'>
+
+        <div className='warning-close-wrapper'>
+          <div className="sql-warning">
+            This SQL reflects the diagram at the time it was generated.
+            <br />
+            Changes in the diagram will
+            <strong> not </strong>
+            update this code.
+          </div>
+
+          <button className="close-btn" onClick={onClose}>✕</button>
+        </div>
+
+        <CodeMirror
+          value={code}
+          extensions={[sql()]}
+          onChange={(value) => setCode(value)}
+          theme='light'
+          height='100%'
+        />
+      </div>
+
+    </div>
+  );
+}
+
+export default CodeEditor;
