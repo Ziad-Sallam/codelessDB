f4fc50557234e88d9083a4e0b058c47adc9fad88
noureldien20 <143279186+noureldien20@users.noreply.github.com>
2025-11-27 07:39:37 +0200

    modified userController tests

--- backend/src/test/java/backend/user/UserControllerTest.java
+++ backend/src/test/java/backend/user/UserControllerTest.java
@@ -18,12 +18,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.*;
@@ -44,6 +47,12 @@ public class UserControllerTest {
 
       @MockitoBean
       private JwtUtil jwtUtil;
+
+      @MockBean
+      private AuthenticationManager authenticationManager;
+
+      @MockBean
+      private PasswordEncoder passwordEncoder;
 
       private static final ObjectMapper mapper = new ObjectMapper();
 
