e70de897441fe469e0696a8ed80f03d8dac0f0ed
ziad sallam <143383350+Ziad-Sallam@users.noreply.github.com>
2025-11-29 20:17:59 +0200

    Delete Backend directory

--- Backend/src/main/java/backend/databaseManagement/UserDatabaseRepository.java
+++
@@ -1,8 +0,0 @@
-package backend.databaseManagement;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import backend.entities.UserDatabase;
-
-@Repository
-public interface UserDatabaseRepository extends JpaRepository<UserDatabase, Integer> {}
