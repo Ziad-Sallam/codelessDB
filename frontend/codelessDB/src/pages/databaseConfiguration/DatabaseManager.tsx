2702b6713e778197d3fa8f8282c3868d6d91d946
Ziad-Sallam <ziadsallam1234@gmail.com>
2025-12-10 14:08:36 +0200

    Update DatabaseManager.tsx

--- frontend/codelessDB/src/pages/databaseConfiguration/DatabaseManager.tsx
+++ frontend/codelessDB/src/pages/databaseConfiguration/DatabaseManager.tsx
@@ -161,12 +161,12 @@ const DatabaseManager: React.FC = () => {
     if (!newName) return ddl;
 
     const HEADER_REGEX =
-      /^\s*CREATE\s+DATABASE[\s\S]*?;\s*USE\s+[\s\S]*?;\s*/i;
+      /^\s*CREATE\s+DATABASE[\s\S]*?;/i;
 
     const rest = ddl.replace(HEADER_REGEX, '');
 
     return (
-      `CREATE DATABASE IF NOT EXISTS ${newName};\n` +
+      `CREATE DATABASE IF NOT EXISTS ${newName};\n\n` +
       rest.trimStart()
     );
   }
