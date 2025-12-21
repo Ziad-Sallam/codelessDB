b09a62854dee433ae584165fa0834d2c937575b5
Ziad-Sallam <ziadsallam1234@gmail.com>
2025-12-21 08:31:08 +0200

    remove empty line

--- backend/src/main/java/backend/config/SecurityConfig.java
+++ backend/src/main/java/backend/config/SecurityConfig.java
@@ -24,7 +24,6 @@ public class SecurityConfig {
     @Autowired
     private GoogleSuccessHandler googleSuccessHandler;
 
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
