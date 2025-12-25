914c24d8f7c1cdea0025377848d668d74faf0566
MahmoudMonir1221 <monirmody1221@gmail.com>
2025-12-25 11:38:54 +0200

    Remove LeftPanel.css and update LeftPanel styles
    Deleted the unused LeftPanel.css file and moved relevant styles directly into the LeftPanel.jsx component using inline styles. This simplifies the component and removes the dependency on the external CSS file.

--- frontend/codelessDB/src/components/LeftPanel.jsx
+++ frontend/codelessDB/src/components/LeftPanel.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import"./styling/LeftPanel.css";
 import {
 	Drawer,
 	Box,
@@ -39,7 +38,7 @@ export default function LeftPanel({ leftNav, setLeftNav }) {
 		>
 			<Box sx={{ display: "flex", gap: 2, alignItems: "center", mb: 2 }}>
 				<Box>
-					<Typography variant="h6" sx={{ fontFamily: 'Poppins', fontWeight: 900, fontSize: "34px" }} className="name">CodeLessDB</Typography>
+					<Typography variant="h6" style={{ fontWeight: "600", fontSize: "28px", fontFamily: 'Poppins', color: "#cadfffff" }}>CodelessDB</Typography>
 					<Typography variant="caption" sx={{ opacity: 1, fontSize: "13px" }}>
 						Shared workspace
 					</Typography>
